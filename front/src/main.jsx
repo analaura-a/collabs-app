c170627c67a9d5f3c9c7ac10bf099b7b790fb8bb
Ana Laura <107949680+analaura-a@users.noreply.github.com>
2023-11-04 19:59:00 -0300

    React Router Dom [Front]

--- front/src/main.jsx
+++ front/src/main.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import App from './App.jsx'
-import './index.css'
+import Error404Page from './pages/Error404Page.jsx'
+
+const routes = createBrowserRouter([
+
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error404Page />,
+    children: [
+      {
+        path: "/",
+        element: <App />,
+      },
+      // {
+      //   path: "/explore",
+      //   element: < ProductsListPage />,
+      // },
+    ]
+  }
+
+])
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={routes} />
   </React.StrictMode>,
 )
---
+++ front/src/pages/Error404Page.jsx
@@ -0,0 +1,10 @@
+const Error404Page = () => {
+    return (
+        <div>
+            <h1>Error 404</h1>
+            <p>Página no encontrada</p>
+        </div>
+    );
+};
+
+export default Error404Page;
