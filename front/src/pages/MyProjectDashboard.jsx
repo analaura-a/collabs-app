98b702e9ab04e7951fb4b70b81b62f6a7ff9aa15
Ana Laura <107949680+analaura-a@users.noreply.github.com>
2023-11-22 02:05:14 -0300

    Mostrar equipo en dashboard de mis proyectos [Front]

--- front/src/pages/MyProjectDashboard.jsx
+++ front/src/pages/MyProjectDashboard.jsx
@@ -1,24 +1,53 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getProject } from "../services/projects.service";
+import { getTeamProjectById } from "../services/projects_teams";
+import UserListItem from "../components/UserListItem";
 
 const MyProjectDashboard = () => {
 
     const [project, setProject] = useState({});
+    const [teamProject, setTeamProject] = useState({
+        "members": []
+    })
+
     const { id } = useParams();
 
     useEffect(() => {
         console.log("Iniciando componente");
 
         getProject(id)
-            .then((data) => setProject(data));
+            .then((data) => setProject(data))
+            .catch(err => console.log(err))
+
+        getTeamProjectById(id)
+            .then((team) => {
+                console.log(team)
+                setTeamProject(team)
+            })
+            .catch(err => console.log(err))
+
     }, []);
 
+    useEffect(() => { }, [teamProject])
+
     return project.name !== undefined ? (
-        
+
         <section className="mt-5">
 
             <h1>Dashboard del proyecto "{project.name}"</h1>
+
+            <h2 className="fw-6 mt-5">Acerca del proyecto</h2>
+            <p>{project.about}</p>
+
+            <h2 className="fw-6 mt-5">Equipo del proyecto</h2>
+            <ul className="mt-2 list-unstyled row row-cols-1 row-cols-md-3 g-4">
+                {
+                    teamProject.members.map((member, index) => (
+                        <UserListItem key={index} user={member}></UserListItem>
+                    ))
+                }
+            </ul>
 
         </section>
 
--- front/src/services/projects_teams.jsx
+++ front/src/services/projects_teams.jsx
@@ -2,12 +2,12 @@ import { call } from "./http.service";
 
 //Obtener equipo de un proyecto particular
 export function getTeamProjectById(id) {
-    return call({ uri: `/projects/${id}/team` })
+    return call({ uri: `projects/${id}/team` })
 }
 
 //Crear un nuevo equipo (asociado a un proyecto)
 export function createTeam(teamData) {
-    return call({ uri: "/project_teams", method: "POST", body: teamData })
+    return call({ uri: "project_teams", method: "POST", body: teamData })
 }
 
 export default {
