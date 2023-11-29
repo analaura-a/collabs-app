26e02714ef104018da06b29fd2bc36b444adef36
Ana Laura <107949680+analaura-a@users.noreply.github.com>
2023-11-28 21:52:32 -0300

    Renderizado condicional en mis proyectos [Front]

--- front/src/pages/MyProjectsPage.jsx
+++ front/src/pages/MyProjectsPage.jsx
@@ -36,27 +36,7 @@ const MyProjectsPage = () => {
 
     useEffect(() => { }, [projects])
 
-    return projects.length != 0 ? (
-        <section className="mt-5">
-
-            <div className="d-md-flex justify-content-between">
-                <h1>Mis proyectos</h1>
-                <Link
-                    to={"/crear-convocatoria"} className="btn btn-primary"
-                >Crear convocatoria para proyecto</Link>
-
-            </div>
-
-            <ul className="list-unstyled row row-cols-1 row-cols-md-3 g-4 mt-5">
-
-                {
-                    projects.map(project => <MyProjectsListItem key={project._id} project={project} />)
-                }
-
-            </ul>
-
-        </section >
-    ) : (
+    return (
 
         <section className="mt-5">
 
@@ -68,12 +48,24 @@ const MyProjectsPage = () => {
 
             </div>
 
-            <p className="mt-5 fs-5 text-center text-secondary">Aún no has creado ninguna convocatoria para proyecto.</p>
+            {projects.length != 0 ? (
+
+                <ul className="list-unstyled row row-cols-1 row-cols-md-3 g-4 mt-5">
+
+                    {
+                        projects.map(project => <MyProjectsListItem key={project._id} project={project} />)
+                    }
+
+                </ul>
+
+            ) : (
+                <p className="mt-5 fs-5 text-center text-secondary">Aún no has creado ninguna convocatoria para proyecto.</p>
+            )}
 
         </section >
+
     )
 
 }
-
 
 export default MyProjectsPage;
